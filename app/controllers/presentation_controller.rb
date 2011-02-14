bffd412106c4aa868a6289ffc9940000d8a9b2a4
Will Fisher <will@gina.alaska.edu>
2011-02-14 11:04:22 -0900

    Initial Commit

---
+++ app/controllers/presentation_controller.rb
@@ -0,0 +1,5 @@
+class PresentationController < ApplicationController
+  def show
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mashups::Application.config.secret_token = 'a4a2a9e22db2dd2c29fb5aae95404520ee69d72dd309e80ff7e80d2ef3d751ae4f28b0b65b367c75e20364e39ed17400904e8f4e76df49a60808df584dfb0acb'
